f279a70453439c0cb1ffcc84424cd0ef1c7e01b6
Deva Aji Saputra <80871396+Devaaji@users.noreply.github.com>
2021-11-10 13:28:29 +0700

    Deploy Portfolio Depdep

--- src/components/aboutme/About.jsx
+++ src/components/aboutme/About.jsx
@@ -29,9 +29,7 @@ function About() {
                         <Skill>Mongodb</Skill>
                     </Skills>
                     <TextProfile>
-                        I’ve spent the past 12+ years working across different areas of digital design; front-end development, email design, marketing site pages, app UI/UX, to my current role designing products for mobile platforms.
-                        These days my time is spent researching, designing, prototyping, and coding. I also help designers get started with their careers.
-                        Out of the office you’ll find me dreaming of soccer, playing bass guitar, and petting all the good dogs.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est error quidem corporis, dolorum nam, aliquam ea, porro expedita possimus nulla fugiat excepturi repudiandae non officiis. Aspernatur rerum ullam nemo alias sit nihil quam iure voluptates, ratione vero nam odit quae!
                     </TextProfile>
                 </BoxProfile>
             </Row>
